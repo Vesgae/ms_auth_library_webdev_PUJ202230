b759fa9c40610720975e0c8192bf5469220dd4e7
Edwin Vesga <43819230+Vesgae@users.noreply.github.com>
2022-10-12 12:24:10 -0500

    FIRST COMMIT - MS auth

---
+++ src/main/java/com/web202230/ms_auth/MsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.web202230.ms_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web202230/ms_auth/MsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web202230.ms_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
